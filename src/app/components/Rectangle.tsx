22bdc3da41586b95860dc5798b37886bb0b31dc5
Samira Hazim <samirahazimr@gmail.com>
2025-02-16 22:43:51 -0600

    Removed files from repository

--- src/app/components/Rectangle.tsx
+++
@@ -1,7 +0,0 @@
-interface RectangleProps {
-    color: string;
-  }
-  
-  export default function Rectangle({ color }: RectangleProps) {
-    return <div style={{ backgroundColor: color }}  className="w-64 h-32"></div>;
-  }
